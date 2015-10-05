9188e897e20c8ca880593ea8b45cedca9ea5c6f7
Terrillo Walls <terrillo@terrillo.me>
2015-10-05 15:30:17 -0500

    e.preventDefault()
    Stop the linking the top of the page

--- timeline.js
+++ timeline.js
@@ -1,9 +1,11 @@
 //document.getElementById('doc').style.display = 'none';
 
-setInterval(function(){
+setInterval(function(e){
     var showmore = document.querySelectorAll(".new-tweets-bar")[0];
     if(showmore) {
         showmore.click();
         document.querySelectorAll(".expand-stream-item").click();
+        return false;
+        e.preventDefault();
     }
 }, 200);
